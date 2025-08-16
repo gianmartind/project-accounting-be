7e668b866c94b30fc1decbfd3e1e7469f0fab164
gmd <gianmartind@gmail.com>
2025-08-16 23:06:32 +0700

    init project

---
+++ src/main/java/com/gmd/project_accounting_be/ProjectAccountingApplication.java
@@ -0,0 +1,13 @@
+package com.gmd.project_accounting_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectAccountingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectAccountingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gmd/project_accounting_be/ProjectAccountingBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gmd.project_accounting_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectAccountingBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
